ee48b9dd940b68f586c97c88826809b9529efc33
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2024-02-05 17:13:33 +0530

    initial commit

---
+++ src/main/java/com/anjaniy/funfactsbackend/FunFactsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anjaniy.funfactsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunFactsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunFactsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anjaniy/funfactsbackend/FunFactsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjaniy.funfactsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunFactsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
